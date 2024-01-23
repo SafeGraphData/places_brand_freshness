64185ff0bdb2eba2485ad8d8c200243c6cfd8246
ilanrich <54144308+ilanrich@users.noreply.github.com>
2024-01-23 15:45:54 -0500

    reverse freshness order

--- places_brand_freshness.py
+++ places_brand_freshness.py
@@ -71,6 +71,8 @@ brand_freshness_30_df["iso_country_code"] = pd.Categorical(
     ordered=True,
 )
 
+brand_freshness_30_df = brand_freshness_30_df.sort_values(by='country_poi_count', ascending=False)
+
 brand_freshness_30_df.rename(
     columns={
         "iso_country_code": "Country Code",
@@ -79,15 +81,16 @@ brand_freshness_30_df.rename(
     },
     inplace=True,
 )
-
-# st.dataframe(brand_freshness_30_df)
+brand_freshness_30_df['Order'] = brand_freshness_30_df['File Age Range'].map({value: index for index, value in enumerate(freshness_list)})
 
 y_range = [0, 100]
+freshness_list = ['120d+', '91-120d', '61-90d', '31-60d', '0-30d'] 
 
 brand_freshness_30 = alt.Chart(brand_freshness_30_df).mark_bar().encode(
     x=alt.X('Country Code', sort=None, title=None),
-    y=alt.Y('Percent of Brands', scale=alt.Scale(domain=y_range), sort='y'),
-    color=alt.Color('File Age Range', scale=alt.Scale(domain=['120d+', '91-120d', '61-90d', '31-60d', '0-30d']))
+      y=alt.Y('Percent of Brands:Q', scale=alt.Scale(domain=y_range)),
+    color=alt.Color('File Age Range:N', scale=alt.Scale(domain=freshness_list)),
+    order = alt.Order('Order:O', sort = 'descending')
 ).properties(
     width=800,
     height=400
